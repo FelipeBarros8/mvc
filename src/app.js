01b4ea834c2eb0572856a53c774eb2a0f044a3a8
FelipeBarros8 <felipebarros@outlook.com.br>
2023-09-15 20:51:51 -0300

    Adicionando conexão com banco mongo db

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express, {json} from 'express';
 import index from './routes/index.js';
+import db from './database/dbConnect.js';
 
 
 const app = express ();
@@ -8,5 +9,10 @@ const app = express ();
 app.use(json());
 app.use("/", index);
 
+
+db.on("error", console.log.bind(console, 'Erro de Conexão'))
+db.once("open", () => {
+    console.log('Conexão com o banco feita com sucesso')
+});
 
 export default app; 
---
+++ src/database/dbConnect.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+mongoose.connect("mongodb+srv://admin:adminadmin123@cluster0.v9fkyer.mongodb.net/ehoje");
+//A função connect guarda a string de conexão dentro dos parênteses.
+
+let db = mongoose.connection;
+//Agora criamos uma varíavel que guarda a ação de conectar nosso ussuário no mongo DB.
+
+export default db;
