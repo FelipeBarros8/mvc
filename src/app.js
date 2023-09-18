a7232ccb985b348119c79b532aca635446a95b9f
FelipeBarros8 <felipebarros@outlook.com.br>
2023-09-18 20:54:33 -0300

    Conexão do banco e criação da API.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
-import express, {json} from 'express';
+import express, { json } from 'express';
 import index from './routes/index.js';
 import db from './database/dbConnect.js';
+import doadores from './routes/projeto.js';
 
 
 const app = express ();
@@ -8,11 +9,12 @@ const app = express ();
 
 app.use(json());
 app.use("/", index);
+app.use("/doadores", doadores);
 
 
 db.on("error", console.log.bind(console, 'Erro de Conexão'))
 db.once("open", () => {
-    console.log('Conexão com o banco feita com sucesso')
+    console.log('Conexão com o banco feita com sucesso.')
 });
 
 export default app; 
